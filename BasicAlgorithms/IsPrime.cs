0ab1ec2b260ee9df9f36a5de6fbf25b47bfffc45
ThaiVu <thai35.tv@gmail.com>
2023-10-12 10:25:56 +0700

    fix IsPrime

--- BasicAlgorithms/IsPrime.cs
+++ BasicAlgorithms/IsPrime.cs
@@ -6,13 +6,13 @@
         public static void CheckNumberIsPrime()
         {
             Console.WriteLine("Check so nguyen to: ");
-            var number = 100;
+            var number = 3;
 
             bool isPrime = true;
             if (number < 2)
                 isPrime = false;
 
-            for (int i = 2; i < number / 2; i++)
+            for (int i = 2; i <= number / 2; i++)
             {
                 if (number % i == 0)
                 {
